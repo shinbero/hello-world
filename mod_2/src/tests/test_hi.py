8f857c3e743794d5eac6a307cb9dbaddc3920560
shinbero <1187932+shinbero@users.noreply.github.com>
2021-10-02 18:22:06 +0900

    edit

--- mod_2/src/tests/test_hi.py
+++ mod_2/src/tests/test_hi.py
@@ -2,6 +2,7 @@ import unittest
 
 import hi as tested_mod
 
+
 class TestAdd2(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         self.tested_func = tested_mod.add_2
@@ -12,5 +13,12 @@ class TestAdd2(unittest.TestCase):
         """引数3を渡した時の動作をテスト."""
         actual = self.tested_func(3)
         expected = 5
+
+        self.assertEqual(actual, expected)
+
+    def test_pass_4(self):
+        """引数3を渡した時の動作をテスト."""
+        actual = self.tested_func(4)
+        expected = 7
 
         self.assertEqual(actual, expected)
