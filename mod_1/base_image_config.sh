9a425227cbf4750e9895226ebf6d9ad0f94e9ea2
shinbero <1187932+shinbero@users.noreply.github.com>
2021-10-02 16:21:22 +0900

    edit

--- mod_1/base_image_config.sh
+++
@@ -1,4 +0,0 @@
-set -u
-
-# 事前にenv_IMAGE_REGISTRY環境変数をセットしてから呼び出すこと.
-export BASE_IMAGE=${env_IMAGE_REGISTRY}/daiana/python:3.9.7-alpine3.14-1.0
---
+++ mod_1/get_base_image.sh
@@ -0,0 +1,19 @@
+set -u
+
+if [[ $# -ne 1  ]]; then
+    echo "Usage: sh get_base_image.sh <Registry server" \
+        "or '' if use docker hub.>" >&2
+    exit 1
+fi
+
+IMAGE_REGISTRY=$1
+
+BASE_IMAGE=daiana/python:3.9.7-alpine3.14-1.0
+
+# 引数でregistryサーバを渡された場合は、registryをimage名の一部とする.
+if [[ ! -z ${IMAGE_REGISTRY} ]]; then
+    BASE_IMAGE=${IMAGE_REGISTRY}/${BASE_IMAGE}
+fi
+
+
+echo "$BASE_IMAGE"
